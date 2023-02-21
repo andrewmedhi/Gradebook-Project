319b63ab5239ad780d5baf3339d6c51970966f11
andrewmedhi <andrew_medeiros@uri.edu>
2023-02-21 16:59:37 -0500

    fix deliverables

--- deliverable.cpp
+++ deliverable.cpp
@@ -1,11 +1,11 @@
 #include "deliverable.h"
 #include <iostream>
 
-Deliverable::Deliverable(std::string name, int points_Worth, int points_Earned, char AssType){
+Deliverable::Deliverable(std::string name, int points_Worth, int points_Earned, char AssType) {
     this->name = name;
     this->points_Worth = points_Worth;
     this->points_Earned = points_Earned;
-    this->AssType = AssType;
+    this->assType = AssType;
 
 
 }
