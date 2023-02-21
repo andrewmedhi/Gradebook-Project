89569be714975fa0610bbfeca22ffbfd6083335e
14014 <ajcote@uri.edu>
2023-02-21 16:43:23 -0500

    we had fun making deliverable changes

--- deliverable.cpp
+++ deliverable.cpp
@@ -1,7 +1,7 @@
 #include "deliverable.h"
 #include <iostream>
 
-deliverable::deliverable(std::string name, int points_Worth, int points_Earned, char AssType){
+Deliverable::Deliverable(std::string name, int points_Worth, int points_Earned, char AssType){
     this->name = name;
     this->points_Worth = points_Worth;
     this->points_Earned = points_Earned;
@@ -10,15 +10,15 @@ deliverable::deliverable(std::string name, int points_Worth, int points_Earned, 
 
 }
 
-std::string deliverable::GetName(){
-    return this.name;
+std::string Deliverable::GetName(){
+    return this->name;
 }
-int deliverable::GetPW(){
-    return this.points_Worth;
+int Deliverable::GetPW(){
+    return this->points_Worth;
 }
-int deliverable::GetPE(){
-    return this.points_Earned;
+int Deliverable::GetPE(){
+    return this->points_Earned;
 }
-char deliverable::GetAss(){
-    return this.assType;
+char Deliverable::GetAss(){
+    return this->assType;
 }
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     std::cout << "Hello, World! >:)" << std::endl;
     std::cout << "Take it EZ BABY" << std::endl;
     std::cout << "I live inside The matrix *>" << std::endl;
-    deliverable weed = new deliverable("Dagoth_Ur", 20, 5,'L' );
+    Deliverable weed = new Deliverable("Dagoth_Ur", 20, 5,'L' );
     std::cout<< weed.GetName();
     //Andrew 8>
     return 0;
