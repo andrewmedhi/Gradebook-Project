763ea8e3106e7b2b78f04a9f9fe82f26aa29618c
andrewmedhi <andrew_medeiros@uri.edu>
2023-02-21 17:04:06 -0500

    fix main

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include "deliverable"
+#include "deliverable.h"
 
 int main() {
     std::cout << "Hello, World! >:)" << std::endl;
     std::cout << "Take it EZ BABY" << std::endl;
     std::cout << "I live inside The matrix *>" << std::endl;
-    Deliverable weed = new Deliverable("Dagoth_Ur", 20, 5,'L' );
+    Deliverable weed ("Dagoth_Ur", 20, 5,'L' );
     std::cout<< weed.GetName();
     std::cout << "I live inside The matrix 8>" << std::endl;
     std::cout << "HI" << std::endl;
+    std::cout << "lol" << std::endl;
     //Andrew 8>
     return 0;
 
