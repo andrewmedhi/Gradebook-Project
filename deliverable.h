ae593534a30b216e7d4d1c57d5dd2875287b78d2
andrewmedhi <andrew_medeiros@uri.edu>
2023-02-21 19:04:32 -0500

    minor fix

--- deliverable.h
+++ deliverable.h
@@ -1,17 +1,18 @@
+#include <string>
 class Deliverable {
 private:
-    std::string name
-    int points_Worth
-    int points_Earned,
-    char assType
+    std::string name;
+    int points_Worth;
+    int points_Earned;
+    char assType;
 
 
 public:
-    Deliverable(std::string name, int points_Worth, int points_Earned, char assType)
-    std::string GetName()
-    int GetPW()
-    int GetPE()
-    char GetAss()
+    Deliverable(std::string name, int points_Worth, int points_Earned, char assType);
+    std::string GetName();
+    int GetPW();
+    int GetPE();
+    char GetAss();
 
 /*
 That looks like a sad face.
